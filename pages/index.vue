4200d4f394ae14237257f833dbd5dba1fd54e94a
Whopper Technologies <65836476+whoppertechnologies@users.noreply.github.com>
2022-02-05 22:33:07 -0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">Hundal Family</h1>
+      <h2 class="subtitle text-center">It's time to fill up our memories</h2>
     </section>
 
     <section class="mt-8">
