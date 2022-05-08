96b096f1e49fbaccd5f9033f91c2180216e81729
amarkantdey <amarkanthdey@gmail.com>
2022-05-08 21:48:04 +0530

    read image from md file

--- components/News.vue
+++ components/News.vue
@@ -33,7 +33,7 @@
                         duration-700
                         ease-out
                       "
-                      src="../assets/images/news-01.jpg"
+                      :src="'../_nuxt/' + post.blogimage"
                       width="352"
                       height="198"
                       alt="News 01"
