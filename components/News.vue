16ddc25325f04dc09f5bf39f887d92c2d4449a74
amarkantdey <amarkanthdey@gmail.com>
2022-05-09 00:01:35 +0530

    defult img

--- components/News.vue
+++ components/News.vue
@@ -33,7 +33,7 @@
                         duration-700
                         ease-out
                       "
-                      :src="'/_nuxt' + post.blogimage"
+                      src="../assets/images/news-01.jpg"
                       width="352"
                       height="198"
                       alt="News 01"
