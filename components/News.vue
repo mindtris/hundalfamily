5643e1603125634bda7f76af22d242dfb2d93ee2
amarkantdey <amarkanthdey@gmail.com>
2022-05-08 22:57:41 +0530

    test

--- components/News.vue
+++ components/News.vue
@@ -33,7 +33,7 @@
                         duration-700
                         ease-out
                       "
-                      :src="'dist/_nuxt' + post.blogimage"
+                      :src="'/_nuxt' + post.blogimage"
                       width="352"
                       height="198"
                       alt="News 01"
