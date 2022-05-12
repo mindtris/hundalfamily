96a25ffb91197cbea7b5acece955136410bff2dc
Whopper Technologies <65836476+whoppertechnologies@users.noreply.github.com>
2022-05-11 19:55:58 -0700

    Update HeroHome.vue

--- components/HeroHome.vue
+++ components/HeroHome.vue
@@ -54,7 +54,7 @@
             Celebrating the past, present and future
           </h1>
           <p
-            class="text-xl text-gray-400 mb-8"
+            class="text-xl text-gray-900 mb-8"
             data-aos="fade-up"
             data-aos-delay="200"
           >
