88ffb5b8e318efa59dcfdd3b9097ee9dab05be42
amarkantdey <amarkanthdey@gmail.com>
2022-05-18 09:49:01 +0530

    aligned social icons

--- components/Header.vue
+++ components/Header.vue
@@ -479,103 +479,104 @@
                   >Contact us</nuxt-link
                 >
               </li>
-              <li>
-                <a
-                  href="https://twitter.com/hundalfamily"
-                  class="
-                    flex
-                    justify-center
-                    items-center
-                    text-amber-400
-                    bg-white
-                    hover:text-gray-100 hover:bg-amber-400
-                    rounded-full
-                    transition
-                    duration-150
-                    ease-in-out
-                    w-fit
-                    mx-auto
-                    mb-1
-                  "
-                  aria-label="Twitter"
-                >
-                  <svg
-                    class="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
+              <ul
+                class="
+                  flex flex-grow
+                  justify-center
+                  mb-2
+                  flex-wrap
+                  items-center
+                "
+              >
+                <li>
+                  <a
+                    href="https://twitter.com/hundalfamily"
+                    class="
+                      flex
+                      justify-center
+                      items-center
+                      text-amber-400
+                      bg-white
+                      hover:text-gray-100 hover:bg-amber-400
+                      rounded-full
+                      transition
+                      duration-150
+                      ease-in-out
+                    "
+                    aria-label="Twitter"
                   >
-                    <path
-                      d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.facebook.com/hundalfamily"
-                  class="
-                    flex
-                    justify-center
-                    items-center
-                    text-amber-400
-                    bg-white
-                    hover:text-gray-100 hover:bg-amber-400
-                    rounded-full
-                    transition
-                    duration-150
-                    ease-in-out
-                    w-fit
-                    mx-auto
-                    mb-1
-                  "
-                  aria-label="Facebook"
-                >
-                  <svg
-                    class="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
+                    <svg
+                      class="w-8 h-8 fill-current"
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li class="ml-4">
+                  <a
+                    href="https://www.facebook.com/hundalfamily"
+                    class="
+                      flex
+                      justify-center
+                      items-center
+                      text-amber-400
+                      bg-white
+                      hover:text-gray-100 hover:bg-amber-400
+                      rounded-full
+                      transition
+                      duration-150
+                      ease-in-out
+                    "
+                    aria-label="Facebook"
                   >
-                    <path
-                      d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/hundalfamily/"
-                  class="
-                    flex
-                    justify-center
-                    items-center
-                    text-amber-400
-                    bg-white
-                    hover:text-gray-100 hover:bg-amber-400
-                    rounded-full
-                    transition
-                    duration-150
-                    ease-in-out
-                    w-fit
-                    mx-auto
-                    mb-1
-                  "
-                  aria-label="Instagram"
-                >
-                  <svg
-                    class="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
+                    <svg
+                      class="w-8 h-8 fill-current"
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li class="ml-4">
+                  <a
+                    href="https://www.instagram.com/hundalfamily/"
+                    class="
+                      flex
+                      justify-center
+                      items-center
+                      text-amber-400
+                      bg-white
+                      hover:text-gray-100 hover:bg-amber-400
+                      rounded-full
+                      transition
+                      duration-150
+                      ease-in-out
+                    "
+                    aria-label="Instagram"
                   >
-                    <circle cx="20.145" cy="11.892" r="1" />
-                    <path
-                      d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z"
-                    />
-                    <path
-                      d="M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z"
-                    />
-                  </svg>
-                </a>
-              </li>
+                    <svg
+                      class="w-8 h-8 fill-current"
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="20.145" cy="11.892" r="1" />
+                      <path
+                        d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z"
+                      />
+                      <path
+                        d="M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
               <!-- <li>
                 <nuxt-link
                   to="/signin"
